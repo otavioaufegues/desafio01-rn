d5df51915e7e3fc2a620fb99eb533ea0c51aeade
otavioaufegues <otavioaufegues@gmail.com>
2022-02-08 09:54:04 -0300

    fix remove

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,19 +20,19 @@ export function Home() {
   }
 
   function handleToggleTaskDone(id: number) {
-    let updatedTasks: Task[] = [...tasks];
+    console.log("done " + id);
+    // let updatedTasks: Task[] = [...tasks];
+    // updatedTasks[id].done
+    //   ? (updatedTasks[id].done = false)
+    //   : (updatedTasks[id].done = true);
 
-    updatedTasks[id].done
-      ? (updatedTasks[id].done = false)
-      : (updatedTasks[id].done = true);
-
-    setTasks(updatedTasks);
+    // setTasks(updatedTasks);
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
-    const newList: Task[] = tasks.filter((task) => tasks.indexOf(task) !== id);
-    setTasks(newList);
+    const newList: Task[] = tasks.filter((task) => task.id !== id);
+    setTasks(newList); 
     //TODO - remove task from state
   }
 
